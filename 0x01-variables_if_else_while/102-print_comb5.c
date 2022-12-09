29478f04488ce0ab18ff9b2c4cb52950ea315b4b
Jerrica1 <jerricajackson93@outlook.com>
2022-12-09 14:49:18 -0800

    102-print_comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,14 +12,14 @@ int main(void)
 	int hun;
 	int ten;
 	int bas;
-for ( tho = 0 ; tho < 10 ; tho++)
+for (tho = 0 ; tho < 10 ; tho++)
 {
 
 	for (hun = 0 ; hun <= 10 ; hun++)
 	{
 		for (ten = 0 ; ten < 10 ; ten++)
 		{
-			for ( bas = 0 ; bas < 10 ; bas++)
+			for (bas = 0 ; bas < 10 ; bas++)
 			{
 				putchar('0' + tho);
 				putchar('0' + hun);
