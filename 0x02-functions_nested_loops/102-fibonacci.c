9aba7bcecf38747dd498daf476b48d595ee3e089
Jerrica1 <jerricajackson93@outlook.com>
2022-12-13 15:08:58 -0800

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,7 +9,7 @@
  * main - Prints first 50 Fibonacci numbers, starting with 1 and 2,
  *	seperated by a comma followed by a space.
  *
- * Return: Always 0 (Success)
+ * Return: Always 0
  */
 int main(void)
 {
