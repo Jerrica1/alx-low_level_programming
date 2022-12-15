575703f083425702e19f693accd840e287fb1931
Jerrica1 <jerricajackson93@outlook.com>
2022-12-15 10:38:44 -0800

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_triangle - draws a right triangle with hypotenuse to the left
+ *
+ * @size: determines both base and height, in rows/columns drawn with '#' tiles
+ *
+ * Return: none
+ */
+
+void print_triangle(int size)
+{
+	int row;
+	int col;
+
+	if (size > 0)
+	{
+		for (row = 0; row < size; row++)
+		{
+			for (col = 0; col < size; col++)
+			{
+				if ((row + col) < (size - 1))
+				{
+					_putchar(' ');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
