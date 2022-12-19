885054421d2fc18ac060bf2b52d5f9416ec45db3
Jerrica1 <jerricajackson93@outlook.com>
2022-12-19 12:19:49 -0800

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,12 +1,10 @@
-#include "main.h"
-
 /**
- * _atoi - Entry point
- * @s: input
- * Return: Always 0 (Success)
+ * _atoi - function that convert a string to an integer.
+ * @s: string
+ * Return: integer
  */
+
 int _atoi(char *s)
 {
-
-	return (0);
+	return (*s);
 }
