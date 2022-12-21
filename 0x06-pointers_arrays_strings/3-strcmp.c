99a18bf3b4c39a1bac92129135c53f6123c804b0
Jerrica1 <jerricajackson93@outlook.com>
2022-12-21 09:23:48 -0800

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,7 +13,7 @@ int _strcmp(char *s1, char *s2)
 	int counter, cmpVal;
 
 	counter = 0;
-	while (s1[counter] == s2[counter] && s1[counter] != '\0')
+	while (s1[counter] == s2[counter] && s2[counter] != '\0')
 	{
 		counter++;
 	}
