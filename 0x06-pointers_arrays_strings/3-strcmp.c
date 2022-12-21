03a040fd65e98072028db5dcb137a275e8db8260
Jerrica1 <jerricajackson93@outlook.com>
2022-12-21 09:37:29 -0800

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,7 +6,7 @@
  *
  * Return: value less than 0 if string is less than the other.
  * value greater than 0 if string is greater than the other.
- *0 if strings are equal.
+ * 0 if strings are equal.
  */
 int _strcmp(char *s1, char *s2)
 {
