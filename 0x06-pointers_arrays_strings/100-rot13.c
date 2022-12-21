13ecc286c90bf738f2ccc3898d5cd931373f76ea
Jerrica1 <jerricajackson93@outlook.com>
2022-12-21 13:15:20 -0800

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,7 +9,7 @@
 char *rot13(char *str)
 {
 	int indx1 = 0, indx2;
-	char alphabet[52] - {'A', 'B', 'C', 'D', 'E', 'F',
+	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
 			     'G', 'H', 'I', 'J', 'K', 'L',
 			     'M', 'N', 'O', 'P', 'Q', 'R',
 			     'S', 'T', 'U', 'V', 'W', 'X',
@@ -18,7 +18,7 @@ char *rot13(char *str)
 			     'k', 'l', 'm', 'n', 'o', 'p',
 			     'q', 'r', 's', 't', 'u', 'v',
 			     'w', 'x', 'y', 'z'};
-	char rot13key[52] - {'N', 'O', 'P', 'Q', 'R', 'S',
+	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
 			     'T', 'U', 'V', 'W', 'X', 'Y',
 			     'Z', 'A', 'B', 'C', 'D', 'E',
 			     'F', 'G', 'H', 'I', 'J', 'K',
