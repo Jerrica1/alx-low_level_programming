b1a3ce55352d623d5cc5767e5cd5119e58d4005c
Jerrica1 <jerricajackson93@outlook.com>
2022-12-22 07:21:20 -0800

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,30 +1,38 @@
 #include "main.h"
-
 /**
- * print_number - prints an arbitrary integer using putchar
+ * print_number - Prints an integer
  *
- * @n: integer to print
+ * @n: input integer
  *
  * Return: void
  */
 void print_number(int n)
 {
-	unsigned int u, i;
-
+	int len, index;
+	len = 1000000000;
 	if (n < 0)
 	{
-		u = -n;
 		_putchar('-');
+	if (n == -2147483648)
+	{
+		index = 1;
+		n += 1;
+	}
+	n = -n;
+	}
+	while (len != 1)
+	{
+		if (n >=  len)
+			_putchar((n / len) % 10 + '0');
+		len /= 10;
+	}
+	if (index == 1)
+	{
+		_putchar(((n % 10) + 1) + '0');
 	}
 	else
 	{
-		u = n;
+		_putchar(n % 10 + '0');
 	}
-
-	i = 1000000000;
-	do {
-		if (i <= u || i == 1)
-			_putchar(u / i % 10 + '0');
-		i /= 10;
-	} while (i != 0);
 }
+
