e26a24f7b7e134231cdf4179372df56b6a640f8b
Jerrica1 <jerricajackson93@outlook.com>
2023-01-08 16:13:28 -0800

    static libraries

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,5 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
+
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -10,14 +12,15 @@ int _strlen(char *s);
 void _puts(char *s);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
-char *_strcat(char *dest, char *Src);
+char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
-char *strncpy(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
-char *_strpnrk(char *s, char *accept);
-char *strstr(char *haystack, char *needle);
-#endif
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+
+#endif /* MAIN_H */
