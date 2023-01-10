7f191323b19d5f5b0700fd7f1b85b20f44b86bd2
Jerrica1 <jerricajackson93@outlook.com>
2023-01-10 07:59:04 -0800

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -74,7 +74,7 @@ char **strtow(char *str)
 	if (words == 0)
 		return (NULL);
 
-	strings = malloc(sizeof(char *) * (words + 1));
+	strinsg = malloc(sizeof(char *) * (words + 1));
 	if (strings == NULL)
 		return (NULL);
 
