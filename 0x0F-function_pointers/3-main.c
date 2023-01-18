0fd8a15bddefe8c99d0c176e5b892d57acf119c5
Jerrica1 <jerricajackson93@outlook.com>
2023-01-18 11:32:49 -0800

    3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,41 +1,46 @@
 #include "3-calc.h"
-#include <stdio.h>
 
 /**
- * main - a basic calculator
- * @argc: the size of the argument vector
- * @argv: the argument vector
+ * main - program that performs simple operations
+ * @argc: number of arguments
+ * @argv: array of arguments
  *
- * Description: This program accepts a number, an operator and another number,
- * in that order. It performs the specified operation and prints the result to
- * standard output (followed by a newline). If called with the wrong number of
- * arguments, it prints "Error" and terminates the process with a status of 98.
- * If the operator given is not one of '+', '-', '*', '/', or '%', it prints
- * "Error" and terminates the process with a status of 99. If asked to perform
- * division by 0 (e.g. using the '/' or '%' operators), it prints "Error" and
- * terminates the process with a status value of 100.
- *
- * Return: Always 0
+ * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
-	int (*f)(int, int);
+	int arg1, arg2, result;
+	char o;
+	int (*func)(int, int);
 
 	if (argc != 4)
 	{
-		puts("Error");
+		printf("Error\n");
 		exit(98);
 	}
 
-	f = get_op_func(argv[2]);
+	arg1 = atoi(argv[1]);
+	arg2 = atoi(argv[3]);
 
-	if (!f)
+	func = get_op_func(argv[2]);
+
+	if (!func)
 	{
-		puts("Error");
+		printf("Error\n");
 		exit(99);
 	}
 
-	printf("%d\n", f(atoi(argv[1]), atoi(argv[3])));
+	o = *argv[2];
+
+	if ((o == '/' || o == '%') && arg2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	result = func(arg1, arg2);
+
+	printf("%\n", result);
 
 	return (0);
 }
