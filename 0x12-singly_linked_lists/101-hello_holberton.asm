02b952bc365df2b5c5cdbbca4b5c060a4020dd2f
Jerrica1 <jerricajackson93@outlook.com>
2023-01-27 15:53:37 -0800

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,19 +1,17 @@
-	section .data
-hello: db "Hello, ALX",10,0
-format: db "%s",0
+section .data
+	fmt db "%s", 10, 0
+	msg db "Hello, ALX", 0
 
 	section .text
-	global main
 	extern printf
+	global main
 
 main:
 	push rbp
-	mov rbp, rsp
-
-	mov rsi, hello
-	mov rdi, format
+	mov rsi, msg
+	mov rdi, fmt
+	mov rax, 0
 	call printf
-
 	pop rbp
 	mov rax, 0
 	ret
