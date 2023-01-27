8eaf9e7c692ff76643d11260b7a021195bc0eeac
Jerrica1 <jerricajackson93@outlook.com>
2023-01-27 15:16:13 -0800

    0-print_list.c

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,15 +1,17 @@
-#ifndef LISTS_H
-#define LISTS_H
-#include <stddef.h>
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
+ * @str: string - (nalloc'ed string)
  * @len: length of the string
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for ALX project
  */
 typedef struct list_s
 {
@@ -18,14 +20,10 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
-int _putchar(char c);
-int _strlen(const char *s);
-
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-void print_before(void) __attribute__ ((constructor));
 
-#endif /* LISTS_H */
+#endif
