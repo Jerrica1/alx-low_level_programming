40ae034d2db4deb1c23326ec8e2a557f7901c59d
Jerrica1 <jerricajackson93@outlook.com>
2023-02-02 11:22:02 -0800

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,32 +1,69 @@
-#include "holberton.h"
+#include "main.h"
 /**
- * binary_to_uint - Entry Point
- * @b: const char
- * Return: 0
+ * _pow - function that return the
+ * value of x raise to the power of y
+ *
+ * @x: parameter to base
+ * @y: parameter of pow
+ *
+ * Return: End program
+ */
+int _pow(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		y--;
+		x = x * _pow(x, y);
+		return (x);
+	}
+
+	return (0);
+}
+/**
+ * binary_to_uint - function that converts a binary number to an
+ * unsigned int
+ *
+ * @b: Pointer with the string whit the number to convert
+ *
+ * Return: The number converted
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int res = 0;
-	int base = 1, i = 0;
+	unsigned int sum = 0;
+	int i, x, n;
 
 	if (b == NULL)
+	{
 		return (0);
-
-	while (b[i + 1])
-	{
-		if (b[i] != '0' && b[i] != '1')
-			return (0);
-		i++;
 	}
 
-	while (i >= 0)
+	n = strlen(b) - 1;
+
+	/* Perform the Operation for the Sum */
+	for (x = 0, i = 0; n >= x; n--)
 	{
-		res += ((b[i] - '0') * base);
-		base *= 2;
-		i--;
+		switch (b[i])
+		{
+			case '1':
+				sum = sum + _pow(2, n);
+				i++;
+				break;
+			case '0':
+				sum = sum + 0;
+				i++;
+				break;
+			default:
+				return (0);
+		}
 	}
 
-
-	return (res);
-
+	return (sum);
 }
