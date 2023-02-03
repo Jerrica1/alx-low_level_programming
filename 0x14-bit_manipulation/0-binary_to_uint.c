4d0778b99dc463509081e8a4e7dd95e0c8962e83
Jerrica1 <jerricajackson93@outlook.com>
2023-02-03 10:53:28 -0800

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,26 +1,33 @@
-#include "main.h"
-
+#include "alx.h"
 /**
- * binary_to_uint - converts binary string to uint
- * @b: string with binary num
- * Return: unsigned int or 0
+ * binary_to_uint - This function convert binary to decimal
+ * @b: This is my string of entry
+ * Return: This return to a digital number
  */
-
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int binary_to_uint(const char *b)
+	unsigned int decimal = 0;
+	int multipilcador = 1, index = 0;
 
-		if (!b)
-			return (0);
-
-	for (num = 0; *b; b++)
+	if (b == NULL)
+		return (0);
+	for (index = 0; b[index] != '\0'; index++)
 	{
-		if (*b == '1')
-			num = (num << 1) | 1;
-		else if (*b == '0')
-			num <<= 1;
-		else
+		if (b[index] != '0' && b[index] != '1')
 			return (0);
 	}
-	return (num);
+	for (index = index - 1; index >= 0; index--)
+	{
+		char currentCharacter;
+
+		currentCharacter = b[index];
+		if (currentCharacter == '1')
+		{
+			decimal += multiplicador;
+		}
+		multiplicador = multiplicador * 2;
+		if (index == 0)
+			break;
+	}
+	return (decimal);
 }
