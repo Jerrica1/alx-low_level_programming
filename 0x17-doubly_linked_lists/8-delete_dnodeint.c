e863ee2b7486ae2de0586d5e8d356c09e42b03be
Jerrica1 <jerricajackson93@outlook.com>
2023-03-16 08:43:35 -0700

    8-delete_dnodeint

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -24,7 +24,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		free(node_to_delete);
 		if (*head)
 			(*head)->prev = NULL;
-		retun (1);
+		return (1);
 	}
 
 	/* search of position to delete */
