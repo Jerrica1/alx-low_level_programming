3ff002d5c93324bd7e18246d0987e86d7fed630c
Jerrica1 <jerricajackson93@outlook.com>
2023-03-16 12:29:49 -0700

    Updated

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -36,8 +36,7 @@ int main(int __attribute__((__unused__)) argc, char *argv[])0
 
 	tmp = 0;
 	for (i = 0; i < len; i++)
-	{
-		if (argv[1][i] > tmp)
+	if (argv[1][i] > tmp)
 			tmp = argv[1][i];
 	}
 	srand(tmp ^ 14);
