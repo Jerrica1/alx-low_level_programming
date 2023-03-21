1ac99020cff8c59f2d0fdd193158c83d0eb210e7
Jerrica1 <jerricajackson93@outlook.com>
2023-03-21 08:32:41 -0700

    main

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,5 +1,8 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
 
 int _putchar(char c);
 int _islower(int c);
@@ -21,5 +24,10 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int mod(int a, int b);
+extern div_t div(int __numer, int __denom);
 
-#endif /* _MAIN_H_ */
+#endif /* MAIN_H */
