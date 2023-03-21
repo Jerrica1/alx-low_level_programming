bc124e0a0910bbb240662b4ae9de96db19a1990e
Jerrica1 <jerricajackson93@outlook.com>
2023-03-21 07:00:16 -0700

    main

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,6 +1,6 @@
-#ifndef DYNAMIC_LIB
-#define DYNAMIC_LIB
-#include <stdlib.h>
+#ifndef MAIN_H
+#define MAIN_H
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -21,4 +21,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+
 #endif /* DYNAMIC_LIB */
