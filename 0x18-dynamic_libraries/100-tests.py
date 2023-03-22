d050a0b99475c18e840a4cb9e731f25f5e33b302
Jerrica Jackson <111321583+Jerrica1@users.noreply.github.com>
2023-03-22 14:06:18 -0500

    Update 100-tests.py

--- 0x18-dynamic_libraries/100-tests.py
+++ 0x18-dynamic_libraries/100-tests.py
@@ -1,9 +1,8 @@
-import random
 import ctypes
 
 cops = ctypes.CDLL('./100-operations.so')
-a = random.randit(-111, 111)
-b = random.randit(-111, 111)
+a = random.randint(-111, 111)
+b = random.randint(-111, 111)
 print("{} + {} = {}".format(a, b, cops.add(a, b)))
 print("{} - {} = {}".format(a, b, cops.sub(a, b)))
 print("{} x {} = {}".format(a, b, cops.mul(a, b)))
