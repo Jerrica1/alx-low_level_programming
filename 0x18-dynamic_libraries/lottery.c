928908ec131a485adb655b8eaa4f271efbed15f1
Jerrica1 <jerricajackson93@outlook.com>
2023-03-24 12:55:12 -0700

    Updated

---
+++ 0x18-dynamic_libraries/lottery.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * printf - prints the lottery
+ * format - the number
+ * Return: always 0
+ */
+
+int printf(const char *format, ...)
+{
+	write(1, "9 8 10 24 75 -9 \n", 17);
+	write(1, "Congratulations, you win the Jackpot!\n", 38);
+	exite(EXIT_FAILURE);
+}
