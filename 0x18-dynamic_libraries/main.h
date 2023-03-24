6a072f048137b72609c4fa973d702fc0ad113484
Jerrica1 <jerricajackson93@outlook.com>
2023-03-24 12:33:08 -0700

    Updated

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -15,14 +15,4 @@ void _puts(char *str);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 char *_strcat(char *dest, char *src);
-char *_strncat(char *dest, char *src, int n);
-char *_strncpy(char *dest, char *src, int n);
-int _strcmp(char *s1, char *s2);
-char *_memset(char *s, char b, unsigned int n);
-char *_memcpy(char *dest, char *src, unsigned int n);
-char *_strchr(char *s, char c);
-unsigned int _strspn(char *s, char *accept);
-char *_strpbrk(char *s, char *accept);
-char *_strstr(char *haystack, char *needle);
-#endif /* DYNAMIC_LIB */
->>>>>>> 5ee29d9791cd8a9a0f5c1930a5e28e37103f6f09
+char *_strnca
