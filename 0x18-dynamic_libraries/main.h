31a42e1c7cd23b963c308fa4d3d67a5e6fc07db1
Jerrica Jackson <111321583+Jerrica1@users.noreply.github.com>
2023-03-24 14:32:10 -0500

    Update main.h

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
-=======
-#ifndef DYNAMIC_LIB
-#define DYNAMIC_LIB
-#include <stdlib.h>
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -11,7 +8,7 @@ int _abs(int n);
 int _isupper(int c);
 int _isdigit(int c);
 int _strlen(char *s);
-void _puts(char *str);
+void _puts(char *s);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 char *_strcat(char *dest, char *src);
@@ -24,5 +21,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-#endif /* DYNAMIC_LIB */
->>>>>>> 5ee29d9791cd8a9a0f5c1930a5e28e37103f6f09
+
+#endif /* _MAIN_H_ */
