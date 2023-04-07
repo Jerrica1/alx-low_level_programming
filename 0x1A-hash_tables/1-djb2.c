28ac8dd2ae09f80cee7aa4bc2082e9d7e8f7de59
Jerrica1 <jerricajackson93@outlook.com>
2023-04-07 15:21:43 -0700

    Hash Tables

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,18 @@
+i#include "hash_tables.h"
+/**
+ * hash_djb2 - dj function
+ * @str: growing up string
+ * Return: index position
+ */
+
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash  = 5381;
+	int c;
+
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	return (hash);
+}
